b1520605e053a4bfec164076cb982f882be1b936
Paweł Lelental <p.lelental@gmail.com>
2017-11-24 20:47:54 +0100

    Add files via upload
    init

---
+++ junction-web/src/main/java/com/hashcode/junctionweb/JunctionWebApplication.java
@@ -0,0 +1,12 @@
+package com.hashcode.junctionweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctionWebApplication.class, args);
+	}
+}
